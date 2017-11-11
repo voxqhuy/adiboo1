1148f049f873a64de660670222965e7ab59819e0
voxqhuy <voxqhuy@gmail.com>
2017-11-11 12:08:27 -0500

    check name

---
+++ js/index.js
@@ -0,0 +1,24 @@
+function checkName(name) {
+
+	// get name from the user
+	// name = document.getElementById("usr").value;
+
+	// if name is right, go to next page; otherwise, return an error
+	if (name.toLowerCase() == "tu" || name.toLowerCase() == "sang") {
+		toSecondPage(name);
+	} else {
+		document.getElementById("nameError").innerHTML = "You may not be allowed here";
+	}
+}
+
+function toSecondPage(name) {
+	alert("Hi " + name + ", How r ya? It's currently under construction. Coming Soon!");
+}
+
+$(document).ready(function() {
+  $('#usr').keypress(function(e) {
+  	if(e.which == 13) {
+  		checkName(this.value)
+  	}
+  });
+});
